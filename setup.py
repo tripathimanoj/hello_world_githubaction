f71e74c77f5b11bd6b0ef7daa24f7003f9c64172
tripathimanoj <tripathimanoj220@gmail.com>
2022-06-06 14:59:36 +0530

    fifth_1 commit

--- setup.py
+++ setup.py
@@ -26,10 +26,3 @@ setup(
 )
 
 
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin <yours repo link>
-git push -u origin main
-git push -f origin main
